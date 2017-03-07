09af5c9c93fb7c9ea933da7ac8843b6132d9fa0f
Владимир Шабанов <koprus77@gmail.com>
2017-03-07 16:11:25 +0300

    add suport for board minions

--- DataStructures.py
+++ DataStructures.py
@@ -1,134 +1,13 @@
-"""Point and Rectangle classes.
-
-This code is in the public domain.
-
-Point  -- point with (x,y) coordinates
-Rect  -- two points, forming a rectangle
-"""
-
 import math, json
 
 
 class Point:
     
-    """A point identified by (x,y) coordinates.
-    
-    supports: +, -, *, /, str, repr
-    
-    length  -- calculate length of vector to point from origin
-    distance_to  -- calculate distance between two points
-    as_tuple  -- construct tuple (x,y)
-    clone  -- construct a duplicate
-    integerize  -- convert x & y to integers
-    floatize  -- convert x & y to floats
-    move_to  -- reset x & y
-    slide  -- move (in place) +dx, +dy, as spec'd by point
-    slide_xy  -- move (in place) +dx, +dy
-    rotate  -- rotate around the origin
-    rotate_about  -- rotate around another point
-    """
     
     def __init__(self, x=0.0, y=0.0):
         self.x = x
         self.y = y
     
-    def __add__(self, p):
-        """Point(x1+x2, y1+y2)"""
-        return Point(self.x+p.x, self.y+p.y)
-    
-    def __sub__(self, p):
-        """Point(x1-x2, y1-y2)"""
-        return Point(self.x-p.x, self.y-p.y)
-    
-    def __mul__( self, scalar ):
-        """Point(x1*x2, y1*y2)"""
-        return Point(self.x*scalar, self.y*scalar)
-    
-    def __div__(self, scalar):
-        """Point(x1/x2, y1/y2)"""
-        return Point(self.x/scalar, self.y/scalar)
-    
-    def __str__(self):
-        return "(%s, %s)" % (self.x, self.y)
-    
-    def __repr__(self):
-        return "%s(%r, %r)" % (self.__class__.__name__, self.x, self.y)
-    
-    def length(self):
-        return math.sqrt(self.x**2 + self.y**2)
-    
-    def distance_to(self, p):
-        """Calculate the distance between two points."""
-        return (self - p).length()
-    
-    def as_tuple(self):
-        """(x, y)"""
-        return (self.x, self.y)
-    
-    def clone(self):
-        """Return a full copy of this point."""
-        return Point(self.x, self.y)
-    
-    def integerize(self):
-        """Convert co-ordinate values to integers."""
-        self.x = int(self.x)
-        self.y = int(self.y)
-    
-    def floatize(self):
-        """Convert co-ordinate values to floats."""
-        self.x = float(self.x)
-        self.y = float(self.y)
-    
-    def move_to(self, x, y):
-        """Reset x & y coordinates."""
-        self.x = x
-        self.y = y
-    
-    def slide(self, p):
-        '''Move to new (x+dx,y+dy).
-        
-        Can anyone think up a better name for this function?
-        slide? shift? delta? move_by?
-        '''
-        self.x = self.x + p.x
-        self.y = self.y + p.y
-    
-    def slide_xy(self, dx, dy):
-        '''Move to new (x+dx,y+dy).
-        
-        Can anyone think up a better name for this function?
-        slide? shift? delta? move_by?
-        '''
-        self.x = self.x + dx
-        self.y = self.y + dy
-    
-    def rotate(self, rad):
-        """Rotate counter-clockwise by rad radians.
-        
-        Positive y goes *up,* as in traditional mathematics.
-        
-        Interestingly, you can use this in y-down computer graphics, if
-        you just remember that it turns clockwise, rather than
-        counter-clockwise.
-        
-        The new position is returned as a new Point.
-        """
-        s, c = [f(rad) for f in (math.sin, math.cos)]
-        x, y = (c*self.x - s*self.y, s*self.x + c*self.y)
-        return Point(x,y)
-    
-    def rotate_about(self, p, theta):
-        """Rotate counter-clockwise around a point, by theta degrees.
-        
-        Positive y goes *up,* as in traditional mathematics.
-        
-        The new position is returned as a new Point.
-        """
-        result = self.clone()
-        result.slide(-p.x, -p.y)
-        result.rotate(theta)
-        result.slide(p.x, p.y)
-        return result
 
 
 class Rect:
@@ -215,3 +94,67 @@ class PlayerHand:
             if i.cardId == cardId:
                 return True
         return False
+
+class Minion:
+    def __init__(self, cardId, id):
+        self.cardId = cardId
+        self.id = id
+        
+    def getName(self):
+        with open('cards.json') as data_file:    
+            data = json.load(data_file)
+        for i in range(len(data)):
+            if "name" in data[i] and data[i]["id"] == self.cardId:
+                return data[i]["name"]
+                break
+        else:
+            return "NONE"
+        
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+        
+class PlayerBoard:
+    def __init__(self):
+        self.minionsList = [0] * 8
+        self.minionsCount = 0
+    
+    def addMinion(self, minion, dstPos):
+        if dstPos == "0":
+            return
+        self.minionsCount += 1
+        dstPos = int(dstPos)
+        if (dstPos == self.minionsCount):
+            self.minionsList[dstPos] = minion
+        else:
+            for i in range(7, dstPos, -1):
+                self.minionsList[i] = self.minionsList[i - 1]
+            self.minionsList[dstPos] = minion 
+                
+    
+        
+    def printMinionsNames(self):
+        for i in range(len(self.minionsList)):
+            if isinstance(self.minionsList[i], Minion):
+                print self.minionsList[i].getName() + " " + str(i)
+        else:
+            print
+            
+    def getMinionIndex(self, minion):
+        #print "Find: (" + str(minion.cardId) + ", " + str(minion.id) + ")"
+        for i in range(len(self.minionsList)):
+            #print "---> (" + str(self.minionsList[i].cardId) + ", " + str(self.minionsList[i].id) + ")"
+            if isinstance(self.minionsList[i], Minion) and self.minionsList[i] == minion:
+                return i
+                break
+        else:
+            return 0
+    
+    def removeMinionByIndex(self, index):
+        if index == 0:
+            return
+        for i in range(index, 7):
+            self.minionsList[i] = self.minionsList[i + 1]
+        self.minionsList[7] = 0
+        
+    def removeMinion(self, minion):
+        self.removeMinionByIndex(self.getMinionIndex(minion))
--- follow.py
+++ follow.py
@@ -31,7 +31,6 @@ file1.seek(os.stat(filename1)[6])
 p_drawCard = r"name=(.*) id=.*to FRIENDLY HAND"
 p_playCreature = r"name=(.*) id=.*FRIENDLY HAND -> FRIENDLY PLAY"
 p_playSpell = r"name=(.*) id=.*from FRIENDLY HAND -> $"
-p_died = r"name=(.*) id=.*from FRIENDLY PLAY -> FRIENDLY GRAVEYARD"
 
 p_toFriendlyHand = r"name=.* id=(?P<id>\d*).* cardId=(?P<cardId>.*) player.*to FRIENDLY HAND"
 p_fromFriendlyHand = r"name=.* id=(?P<id>\d*).* cardId=(?P<cardId>.*) player.*from FRIENDLY HAND"
@@ -40,7 +39,13 @@ p_posNumberChange = r".*cardId=(.*) player=1] pos from (. -> .)"
 
 p_turnStart = r"GameState.*tag=TURN value=(?P<number>\d*)"
 
+
+p_minionPlay = r"ZoneMgr.CreateLocalChangesFromTrigger().*name=.* id=(?P<id>\d*).* cardId=(?P<cardId>.*) player.*dstZoneTag=PLAY dstPos=(?P<dstPos>\d)"
+p_died = r"name=.* id=(?P<id>\d*).* cardId=(?P<cardId>.*) player.*from FRIENDLY PLAY -> FRIENDLY GRAVEYARD"
+p_minionChangePosition = r"ZoneMgr.*id=(?P<id>\d*).*zone=PLAY.*zonePos (?P<from>\d*) -> (?P<to>\d*)"
+
 playerHand = PlayerHand()
+playerBoard = PlayerBoard()
 
 isGotCoin = False
 
@@ -60,13 +65,23 @@ while 1:
         toFriendlyHand = re.search(p_toFriendlyHand, line)
         fromFriendlyHand = re.search(p_fromFriendlyHand, line)
         turnStart = re.search(p_turnStart, line)
+        minionPlay = re.search(p_minionPlay, line)
+        minionChangePosition = re.search(p_minionChangePosition, line)
+        died = re.search(p_died, line)
+        
+        if died:
+            playerBoard.removeMinion(Minion(died.group("cardId"), died.group("id")))
+            playerBoard.printMinionsNames()
+        if minionPlay:
+            playerBoard.addMinion(Minion(minionPlay.group("cardId"), minionPlay.group("id")), minionPlay.group("dstPos"))
+            playerBoard.printMinionsNames()
         
         if toFriendlyHand:
             playerHand.addCard(Card(toFriendlyHand.group("cardId"), toFriendlyHand.group("id")))
-            #playerHand.printCardsNames()
+            playerHand.printCardsNames()
         if fromFriendlyHand:
             playerHand.removeCard(Card(fromFriendlyHand.group("cardId"), fromFriendlyHand.group("id")))
-            #playerHand.printCardsNames()
+            playerHand.printCardsNames()
             
         if (playerHand.isInHand("GAME_005")):
             isGotCoin = True
@@ -74,5 +89,5 @@ while 1:
             #print "Turn start"
             if ((isGotCoin and int(turnStart.group("number")) % 2 == 0) or ((not isGotCoin) and int(turnStart.group("number")) % 2 == 1)):
                 if (int(turnStart.group("number"))) / 2 >= 1:
-                    thread.start_new_thread(sleepAndMoveToCard, (playerHand.cardsList[0], playerHand))
+                    pass#thread.start_new_thread(sleepAndMoveToCard, (playerHand.cardsList[0], playerHand))
                 print "YOUR TURN!!! Number: " + str(int(turnStart.group("number")) / 2)# + "   //" + turnStart.group("number")
