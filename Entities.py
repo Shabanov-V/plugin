d7ec387668ccff038be585cea3327ffbc0e1aa7d
Владимир Шабанов <koprus77@gmail.com>
2017-05-13 18:27:48 +0300

    all board minions display now

--- Entities.py
+++ Entities.py
@@ -128,18 +128,22 @@ class myHandCards(IAlterationEntity):
         changed_position = re.search(regExps.change_card_position, logLine)
         if played_card:
             self.del_card_from_hand(played_card.group("id"))
-            self.debug_print_shit()
+            #self.debug_print_shit()
         if drawed_card:
             self.add_card_to_hand(Card(drawed_card.group("cardId"), drawed_card.group("id")))
-            self.debug_print_shit()
+            #self.debug_print_shit()
         if changed_position:
             self.change_card_position(changed_position.group("id"), int(changed_position.group("pos_2")))
             
             
 class board(IAlterationEntity):
     minions = [Minion(None, None)] * 10
-    def __init__(self):
+    playerNumb = 0
+    isOpponent = 0
+    
+    def __init__(self, t):
         self.minions = [Minion(None, None)] * 10
+        self.isOpponent = t
         
     def addMinion(self, minion, dstPos):
         if minion.type != "MINION":
@@ -195,17 +199,29 @@ class board(IAlterationEntity):
         self.addMinion(t, dstPos)
     
     def check_n_change(self, logLine):
-        minionPlay = re.search(regExps.minionPlay2, logLine)
-        minionChangePosition = re.search(regExps.minionChangePosition1, logLine)
+        friendlyMinionPlay = re.search(regExps.friendly_minion_play2, logLine)
+        minionChangePosition = re.search(regExps.minion_change_position2, logLine)
+        oppositeMinionPlay = re.search(regExps.opposite_minion_play, logLine)
         died = re.search(regExps.died, logLine)
+        whoIsWho1 = re.search(regExps.who_is_who1, logLine)
+        whoIsWho2 = re.search(regExps.who_is_who2, logLine)
+        if whoIsWho1 and self.isOpponent == 0:
+            self.playerNumb = int(whoIsWho1.group("player_numb"))
+        if whoIsWho2 and self.isOpponent == 1:
+            self.playerNumb = int(whoIsWho2.group("player_numb"))
         if died:
-            self.removeMinion(Minion(died.group("cardId"), int(died.group("id"))))
-            self.debug_print_shit()
-        if minionPlay:
-            self.addMinion(Minion(minionPlay.group("cardId"), minionPlay.group("id")), minionPlay.group("dstPos"))
-            print "lol"
-            self.debug_print_shit()
-        if minionChangePosition:
+            self.removeMinion(Minion(died.group("cardId"), (died.group("id"))))
+            #print "111: " + died.group("cardId") + " " + died.group("id")
+            #self.debug_print_shit()
+        if friendlyMinionPlay and self.isOpponent == 0:
+            self.addMinion(Minion(friendlyMinionPlay.group("cardId"), friendlyMinionPlay.group("id")), friendlyMinionPlay.group("dstPos"))
+            #print "lol"
+            #self.debug_print_shit()
+        if oppositeMinionPlay and self.isOpponent == 1:
+            self.addMinion(Minion(oppositeMinionPlay.group("cardId"), oppositeMinionPlay.group("id")), oppositeMinionPlay.group("dstPos"))
+            #print "omg"
+            #self.debug_print_shit()
+        if minionChangePosition and self.playerNumb == int(minionChangePosition.group("player")):
             self.change_position(minionChangePosition.group("id"), int(minionChangePosition.group("dstPos")))
-            print "lol"
-            self.debug_print_shit()
+            #print "wtf"
+            #self.debug_print_shit()
--- RegExps.py
+++ RegExps.py
@@ -10,11 +10,20 @@ class regExps():
     player_name_n_id = r".*id=(?P<id>\d*).*name=(?P<name>\w*).*tag=NUM_CARDS_DRAWN_THIS_TURN.*"
     # Power reg exps
     hero_power_activations = ".*Entity=(name).*tag=HEROPOWER_ACTIVATIONS_THIS_TURN value=(?P<value>\d*)" # Dont forget to replace (name) => player_name
-    died = r"name=.* id=(?P<id>\d*).* cardId=(?P<cardId>.*) player.*from FRIENDLY PLAY -> FRIENDLY GRAVEYARD"
+    died = r"name=.* id=(?P<id>\d*).* cardId=(?P<cardId>.*) player.*from .*PLAY -> .*GRAVEYARD"
     minionChangePosition = r"ZoneMgr.*id=(?P<id>\d*).*zone=PLAY.*zonePos (?P<from>\d*) -> (?P<to>\d*)"
     minionPlay1 = r"PowerTaskList.DebugPrintPower.*TAG_CHANGE Entity=\[name=.* id=(?P<id>\d*).*zonePos=(?P<dstPos>\d).*cardId=(?P<cardId>.*) player=.\] tag=ZONE value=PLAY"
     minionChangePosition1 = r"PowerTaskList.DebugPrintPower.*TAG_CHANGE Entity=\[name=.* id=(?P<id>\d*).*cardId=(?P<cardId>.*) player=.\] tag=ZONE_POSITION value=(?P<dstPos>\d)"
     
-    minionPlay2 = r"ZoneChangeList.ProcessChanges.*TRANSITIONING card \[name=(.+) id=(?P<id>\d+) zone=.* zonePos=(?P<dstPos>\d) cardId=(?P<cardId>.*) player=.\].* to FRIENDLY PLAY"
-    minionChangePosition2 = r"ZoneChangeList.ProcessChanges.*id=\d+ .* \[name=.* id=(?P<id>\d*) zone=.* zonePos=(\d) cardId=(?P<cardId>.*) player=.\] pos from \d -> (?P<dstPos>\d)"
+    friendly_minion_play2 = r"ZoneChangeList.ProcessChanges.*TRANSITIONING card \[name=(.+) id=(?P<id>\d+) zone=.* zonePos=(?P<dstPos>\d) cardId=(?P<cardId>.*) player=.\].* to FRIENDLY PLAY"
+    minion_change_position2 = r"ZoneChangeList.ProcessChanges.*id=\d+ .* \[name=.* id=(?P<id>\d*) zone=.* zonePos=(\d) cardId=(?P<cardId>.*) player=(?P<player>.)\] pos from \d -> (?P<dstPos>\d)"
     
+    opposite_minion_play = r"ZoneChangeList.ProcessChanges.*TRANSITIONING card \[name=(.+) id=(?P<id>\d+) zone=.* zonePos=(?P<dstPos>\d) cardId=(?P<cardId>.*) player=.\].* to OPPOSING PLAY"
+    
+    who_is_who1 = r"player=(?P<player_numb>.)].*FRIENDLY"
+    who_is_who2 = r"player=(?P<player_numb>.)].*OPPOSING"
+    """ ZoneChangeList.ProcessChanges.*TRANSITIONING card \[name=(.+) id=(\d+) zone=.* zonePos=(\d) cardId=(.*) player=1\].* to FRIENDLY PLAY """
+    
+    """ ZoneChangeList.ProcessChanges.*id=\d+ .* \[name=.* id=(\d*) zone=.* zonePos=(\d) cardId=(.*) player=(.)\] pos from \d -> (\d) """
+    
+    """ name=.* id=(\d*).* cardId=(.*) player.*from .*PLAY -> .*GRAVEYARD """
